7727fea96ba6ce3edbeb4c00d4ea6a7c1d50b7b5
leecoder <hj.lee@explzn.com>
2022-12-06 09:36:43 +0900

    (feat) : swagger 추가 및 CORS 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);  
+
+  app.enableCors({
+    origin: 'http://localhost:3500',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT);
 }
 
